644f82f268f8a6adf4f6fe19149113e932cce87f
XTR\anugrah.sakti <anugrah.sakti@gmail.com>
2023-10-14 14:13:26 +0700

    render login/signup with outlet tag

--- react/src/components/GuestLayout.jsx
+++ react/src/components/GuestLayout.jsx
@@ -1,7 +1,10 @@
+import { Outlet } from "react-router-dom";
+
 export default function GuestLayout(){
     return (
-        <>
-            
-        </>
+        <div>
+            {/* render login/signup by using Outlet*/}
+            <Outlet />
+        </div>
     )
 }
