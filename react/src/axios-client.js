72c467c307121fed0dba5056fb8a7d2f2b9a4fef
XTR\anugrah.sakti <anugrah.sakti@gmail.com>
2023-10-14 18:34:54 +0700

    add interceptors before send req and after res receive

--- react/src/axios-client.js
+++ react/src/axios-client.js
@@ -5,4 +5,30 @@ const axiosClient = axios.create({
     baseURL: `${import.meta.env.VITA_APP_BASE_URL}/api`
 })
 
+// add req intercept, before send req
+axiosClient.interceptors.request.use((config) => {
+    const token = localStorage.get('ACCESS_TOKEN')
+
+    // modify config
+    config.headers.Authorization = `Bearer ${token}`
+
+    return config;
+})
+
+// add req intercept, after response receive
+axiosClient.interceptors.response.use((response) => {
+    // result
+    return response;
+}, (error) => {
+    // rejected
+    const { response } = error;
+
+    if (response.status === 401) {
+        // token invalid/exp token
+        localStorage.removeItem('ACCESS_TOKEN');
+    }
+
+    throw error;
+})
+
 export default axiosClient;
