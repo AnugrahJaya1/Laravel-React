c79d155f6d97749ed1882523768ec9e4702c7304
XTR\anugrah.sakti <anugrah.sakti@gmail.com>
2023-10-14 15:56:13 +0700

    add UI for default layout

--- react/src/components/DefaultLayout.jsx
+++ react/src/components/DefaultLayout.jsx
@@ -1,17 +1,35 @@
-import { Navigate, Outlet } from "react-router-dom";
+import { Link, Navigate, Outlet } from "react-router-dom";
 import { useStateContext } from "../contexts/ContextProvider.jsx"
 
-export default function DefaultLayout(){
-    const {user, token} = useStateContext();
+export default function DefaultLayout() {
+    const { user, token } = useStateContext();
 
-    if(!token){
-        return <Navigate to="/login"/>
+    if (!token) {
+        return <Navigate to="/login" />
     }
 
     return (
-        <div>
-             {/* render children by using Outlet*/}
-             <Outlet />
+        <div id="defaultLayout">
+            {/* side bar */}
+            <aside>
+                <Link to="/dashboard">Dashboard</Link>
+                <Link to="/users">Users</Link>
+            </aside>
+
+            <div className="content">
+                <header>
+                    <div>
+                        Header
+                    </div>
+                    <div>
+                        User Info
+                    </div>
+                </header>
+                <main>
+                    {/* render children by using Outlet*/}
+                    <Outlet />
+                </main>
+            </div>
         </div>
     )
 }
