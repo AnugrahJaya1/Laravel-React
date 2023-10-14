ac7c42251a761075c64656d21cb51066ad639b05
XTR\anugrah.sakti <anugrah.sakti@gmail.com>
2023-10-14 14:10:14 +0700

    update route so can use multiple layouts (components

--- react/src/router.jsx
+++ react/src/router.jsx
@@ -1,26 +1,46 @@
-import {createBrowserRouter} from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import Login from "./views/Login";
 import Signup from "./views/Signup";
 import Users from "./views/Users";
 import NotFound from "./views/NotFound";
+import DefaultLayout from "./components/DefaultLayout";
+import GuestLayout from "./components/GuestLayout";
 
 // define react route
 const router = createBrowserRouter([
     {
-        path: '/login',
-        element: <Login />
+        path: '/',
+        element: <DefaultLayout />,
+        // default layout have children
+        children: [
+            {
+                path: '/dashboard',
+                element: <Users />
+            },
+            {
+                path: '/users',
+                element: <Users />
+            },
+        ]
     },
     {
-        path: '/signup',
-        element: <Signup />
-    },
-    {
-        path: '/users',
-        element: <Users />
+        path: '/',
+        element: <GuestLayout />,
+        // guest layout have children
+        children: [
+            {
+                path: '/login',
+                element: <Login />
+            },
+            {
+                path: '/signup',
+                element: <Signup />
+            },
+        ]
     },
     {
         path: '*',
-        element: <NotFound/>
+        element: <NotFound />
     },
 ])
 
