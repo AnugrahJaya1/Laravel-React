7c95ec51d1cbc21623b7a67c8c73c5538dbb835b
XTR\anugrah.sakti <anugrah.sakti@gmail.com>
2023-10-14 16:16:46 +0700

    move code so that there is no duplication

--- react/src/views/Login.jsx
+++ react/src/views/Login.jsx
@@ -1,26 +1,22 @@
 import { Link } from "react-router-dom";
 
-export default function Login(){
+export default function Login() {
     // ev = event
     const onSubmit = (ev) => {
         ev.preventDefault();
     }
 
     return (
-        <div className="login-signup-form animated fadeInDown">
-            <div className="form">
-                <form onSubmit={onSubmit}>
-                    <h1 className="title">
-                        Login into your account
-                    </h1>
-                    <input type="email" placeholder="Email" />
-                    <input type="password" placeholder="Password"/>
-                    <button className="btn btn-block">Login</button>
-                    <p className="message">
-                        Not Register? <Link to="/signup">Create an account</Link>
-                    </p>
-                </form>
-            </div>
-        </div>
+        <form onSubmit={onSubmit}>
+            <h1 className="title">
+                Login into your account
+            </h1>
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <button className="btn btn-block">Login</button>
+            <p className="message">
+                Not Register? <Link to="/signup">Create an account</Link>
+            </p>
+        </form>
     )
 }
