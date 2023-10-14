430d800301487073e88d0c1cf6553e8dd65b3a0e
XTR\anugrah.sakti <anugrah.sakti@gmail.com>
2023-10-14 20:03:46 +0700

    mend
    show error message from req validation

--- react/src/views/Signup.jsx
+++ react/src/views/Signup.jsx
@@ -37,7 +37,6 @@ export default function Signup() {
             .catch(err => {
                 const response = err.response;
                 if(response && response.status === 422){
-                    console.log(response.data.errors)
                     setErrors(response.data.errors)
                 }
             })
