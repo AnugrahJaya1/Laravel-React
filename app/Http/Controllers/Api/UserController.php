3749efa5d35e1e79af758e8b555481e16642b9f0
XTR\anugrah.sakti <anugrah.sakti@gmail.com>
2023-10-15 11:35:25 +0700

    add logic for index function, get all user

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -6,15 +6,19 @@ use App\Http\Controllers\Controller;
 use App\Models\User;
 use App\Http\Requests\StoreUserRequest;
 use App\Http\Requests\UpdateUserRequest;
+use App\Http\Resources\UserResource;
 
 class UserController extends Controller
 {
     /**
      * Display a listing of the resource.
+     * @return \Illuminate\Http\Resources\Json\AnonymousResourceCollection
      */
     public function index()
     {
-        //
+        return UserResource::collection(
+            User::query()->orderBy('id', 'desc')->paginate(10)
+        );
     }
 
     /**
