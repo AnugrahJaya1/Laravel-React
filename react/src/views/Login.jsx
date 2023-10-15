74bc286e0fd6b4cfb77d1492c5ae70a2081c31f2
XTR\anugrah.sakti <anugrah.sakti@gmail.com>
2023-10-15 10:09:20 +0700

    handle request

--- react/src/views/Login.jsx
+++ react/src/views/Login.jsx
@@ -1,16 +1,41 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { Link } from "react-router-dom";
+import axiosClient from "../axios-client";
+import { useStateContext } from "../contexts/ContextProvider";
 
 export default function Login() {
     const emailRef = useRef();
     const passwordRef = useRef();
 
+    // state context
+    const { setUser, setToken } = useStateContext()
+
     // set error message
-    const [errors, setErrors] =  useState(null)
+    const [errors, setErrors] = useState(null)
 
     // ev = event
     const onSubmit = (ev) => {
         ev.preventDefault();
+
+        const payload = {
+            email: emailRef.current.value,
+            password: passwordRef.current.value,
+        }
+
+        console.log(payload);
+
+        // make req to server
+        axiosClient.post('/login', payload)
+            .then(({ data }) => { // response
+                setUser(data.user)
+                setToken(data.token)
+            })
+            .catch(err => {
+                const response = err.response;
+                if (response && response.status === 422) {
+                    setErrors(response.data.errors)
+                }
+            })
     }
 
     return (
@@ -18,8 +43,8 @@ export default function Login() {
             <h1 className="title">
                 Login into your account
             </h1>
-            <input type="email" placeholder="Email" />
-            <input type="password" placeholder="Password" />
+            <input ref={emailRef} type="email" placeholder="Email" />
+            <input ref={passwordRef} type="password" placeholder="Password" />
             <button className="btn btn-block">Login</button>
             <p className="message">
                 Not Register? <Link to="/signup">Create an account</Link>
