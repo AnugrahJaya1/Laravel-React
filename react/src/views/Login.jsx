8f7422b81d382f6fa131ccc49459c77b29f7bd33
XTR\anugrah.sakti <anugrah.sakti@gmail.com>
2023-10-15 10:04:39 +0700

    add variable for email, pass, and error

--- react/src/views/Login.jsx
+++ react/src/views/Login.jsx
@@ -1,6 +1,13 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Login() {
+    const emailRef = useRef();
+    const passwordRef = useRef();
+
+    // set error message
+    const [errors, setErrors] =  useState(null)
+
     // ev = event
     const onSubmit = (ev) => {
         ev.preventDefault();
