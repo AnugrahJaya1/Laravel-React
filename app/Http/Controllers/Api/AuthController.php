6f14f3879fb5e9b2eb2839ae751787992e0af697
XTR\anugrah.sakti <anugrah.sakti@gmail.com>
2023-10-15 10:20:58 +0700

    add status code

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -33,7 +33,7 @@ class AuthController extends Controller
         if (!Auth::attempt($credential)) {
             return response([
                 'message' => 'Provided email address or password is incorrect'
-            ]);
+            ], 422);
         }
 
         /** @var User user */
